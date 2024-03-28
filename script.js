df9586ef445d9cd10d0b388b475139757008b590
kammmmow <kamoda.yohei@gmail.com>
2024-03-29 00:37:32 +0900

    pic move edit (Kamoda commit)

--- script.js
+++ script.js
@@ -58,3 +58,45 @@ $(function(){
     $(".img-seminar5 img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".img-seminar5");
   },5000);
 });
+
+$(function(){
+  $(".img-seminar6 img:not(:first-child)").hide();
+  setInterval(function() {
+    $(".img-seminar6 img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".img-seminar6");
+  },5000);
+});
+
+$(function(){
+  $(".img-seminar7 img:not(:first-child)").hide();
+  setInterval(function() {
+    $(".img-seminar7 img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".img-seminar7");
+  },5000);
+});
+
+$(function(){
+  $(".img-seminar7 img:not(:first-child)").hide();
+  setInterval(function() {
+    $(".img-seminar7 img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".img-seminar7");
+  },5000);
+});
+
+$(function(){
+  $(".img-seminar8 img:not(:first-child)").hide();
+  setInterval(function() {
+    $(".img-seminar8 img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".img-seminar8");
+  },5000);
+});
+
+$(function(){
+  $(".img-seminar9 img:not(:first-child)").hide();
+  setInterval(function() {
+    $(".img-seminar9 img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".img-seminar9");
+  },5000);
+});
+
+$(function(){
+  $(".img-seminar10 img:not(:first-child)").hide();
+  setInterval(function() {
+    $(".img-seminar10 img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".img-seminar10");
+  },5000);
+});
